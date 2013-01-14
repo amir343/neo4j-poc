e95dfafed3233de4c778e04c9e114c59cb8f1338
Anders Eriksson <anders.eriksson@gmail.com>
2013-01-15 00:16:07 +0100

    added future level

--- src/main/scala/com/jayway/kperson/Migration.scala
+++ src/main/scala/com/jayway/kperson/Migration.scala
@@ -9,7 +9,7 @@ import org.neo4j.graphdb.{RelationshipType, Node}
 sealed abstract class PersonNode {
   def name:String
 }
-case class Skill(name:String, level:Int) extends PersonNode
+case class Skill(name:String, level:Int, future_level:Int) extends PersonNode
 case class People(id:Int, firstName:String, lastName:String, skills:List[Skill] = List.empty[Skill]) extends PersonNode {
   override val name:String = s"$firstName $lastName"
 }
@@ -18,10 +18,15 @@ object KNOWS extends RelationshipType {
   val name = "KNOWS"
 }
 
+object WANT_TO_KNOW extends RelationshipType {
+  val name = "WANT_TO_KNOW"
+}
+
+
 class KPersonMySql {
 
   private implicit val getPeopleResult = GetResult( r => People(r.nextInt(), r.nextString(), r.nextString()))
-  private implicit val getSkillResult = GetResult( r => Skill(r.nextString(), r.nextInt()))
+  private implicit val getSkillResult = GetResult( r => Skill(r.nextString(), r.nextInt(), r.nextInt()))
 
   private val peopleWithSkills = mutable.ListBuffer[People]()
 
@@ -30,12 +35,12 @@ class KPersonMySql {
     val people:Iterator[People] = Q.queryNA[People]("SELECT id, first_name, last_name FROM people").elements()
 
     people foreach { p =>
-      peopleWithSkills += p.copy( skills =  getSkills(p))
+      peopleWithSkills += p.copy( skills = getSkills(p))
     }
 
     def getSkills(people:People):List[Skill] = {
       val skills = mutable.ListBuffer[Skill]()
-      Q.queryNA[Skill](s"SELECT name, level FROM skills s JOIN skill_types t ON s.skill_type_id=t.id WHERE s.person_id=${people.id}") foreach {
+      Q.queryNA[Skill](s"SELECT name, level, future_level FROM skills s JOIN skill_types t ON s.skill_type_id=t.id WHERE s.person_id=${people.id}") foreach {
         s => skills += s
       }
       skills.toList
@@ -55,9 +60,17 @@ class NeoMigration {
     employees foreach { emp =>
       val empNode = createNode(emp)
       emp.skills foreach { skill =>
-        val skillNode = createNode(skill)
-        val rel = empNode.createRelationshipTo(skillNode, KNOWS)
-        rel.setProperty("level", skill.level)
+        if ( skill.level > 0  || skill.future_level > 0) {
+          val skillNode = createNode(skill)
+          if (skill.level > 0) {
+            val rel = empNode.createRelationshipTo(skillNode, KNOWS)
+            rel.setProperty("level", skill.level.intValue())
+          }
+          if (skill.future_level > 0) {
+            val rel2 = empNode.createRelationshipTo(skillNode, WANT_TO_KNOW)
+            rel2.setProperty("level", skill.future_level.intValue())
+          }
+        }
       }
     }
   }
